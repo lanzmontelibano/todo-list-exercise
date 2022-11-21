461c6998914bc8da91b7cc86dbcf35f095148942
lanzmontelibano <118333208+lanzmontelibano@users.noreply.github.com>
2022-11-21 10:20:56 +0800

    added draggable

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,38 +5,54 @@
         <label>Add Item </label>
         <input
           v-model="newTodo"
-          name="addTodo">
-        <button>Add</button>    
+          name="addTodo"
+        >
+        <button>Add</button>  
     </form>
+    <button @click="markAllDone()">Mark all done</button>
+    <button @click="unMarkAllDone()">Unmark all done</button>
   </div>
-  <button @click="markAllDone()">Mark all done</button>
-  <button @click="unMarkAllDone()">Unmark all done</button>
   <div class="todoList">
-    <div v-for="(todo, index) in todoTasks" :key="index">
-      <div v-if="todo.editing" class="todoItem" draggable="true">
-        <label>Edit Text</label>
-        <input
-          v-model="updatedTodo"
-          name="updateTodo">
-        <button @click="updateTodo(index, updatedTodo)">Update</button>
-        <button @click="doEdit(index)">Cancel</button>    
+      <div>
+        <draggable v-model="todoTasks" @start="true" @end="false" item-key="index">
+        <template #item="{element, index}">
+        <div v-if="element.editing" class="todoItem">
+          <label>Edit Text</label>
+          <input
+            v-model="updatedTodo"
+            name="updateTodo"
+          >
+          <button @click="updateTodo(index, updatedTodo)">Update</button>
+          <button @click="doEdit(index)">Cancel</button>    
+        </div>
+        
+        <div 
+            v-else 
+            class="todoItem" 
+            :class="{ todoItemDone: element.done }"
+          >
+          <h3 :class="{ done: element.done }">{{ element.content }}</h3>
+          <button @click="toggleDone(index)">Mark</button>
+          <button @click="removeTodo(index)">Remove</button>
+          <button v-if="!element.done" @click="doEdit(index)">Edit</button>
+        </div> 
+        </template>
+      </draggable>
       </div>
-      <div v-else class="todoItem" draggable="true" :class="{ todoItemDone: todo.done }">
-        <h3 :class="{ done: todo.done }">{{ todo.content }}</h3>
-        <button @click="toggleDone(index)">Mark</button>
-        <button @click="removeTodo(index)">Remove</button>
-        <button v-if="!todo.done" @click="doEdit(index)">Edit</button>
-      </div> 
-    </div>
   </div>
 </template>
 
 <script lang="ts">
+import draggable from 'vuedraggable'
 import { ref } from 'vue'
 import ITodoTasks from '@/types/todoTasks'
 
 export default{
+  components: {
+    draggable
+  },
   setup() {
+    
     const newTodo = ref('')
     const updatedTodo = ref('')
     const todoTasks = ref<ITodoTasks[]>([])
@@ -60,7 +76,7 @@ export default{
 
     function updateTodo(index: number, w: string) {
       todoTasks.value[index].content = w
-      todoTasks.value[index].editing = false
+      todoTasks.value[index].editing = !todoTasks.value[index].editing
       updatedTodo.value = ''
     }
 
@@ -89,5 +105,6 @@ export default{
       doEdit
     }
   }
+  
 }
 </script>
